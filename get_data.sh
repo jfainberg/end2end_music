4938b74bb8306bbc0f137e387b9dd8bb86bd57a4
jfainberg <joachimfainberg@gmail.com>
2019-06-16 18:23:40 +0100

    Use more data

--- get_data.sh
+++ get_data.sh
@@ -21,6 +21,12 @@ fi
 
 
 mkdir -p wav
-ls data/a/*.mp3 | xargs -P 10 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+ls data/a/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+ls data/b/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+ls data/c/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+ls data/d/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+ls data/e/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+ls data/f/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+
 
 echo "$0: Data downloaded successfully"
