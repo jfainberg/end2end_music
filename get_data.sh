62e1c3d94b14ad1f9c12f526327932b4a77f9dd9
jfainberg <joachimfainberg@gmail.com>
2019-06-17 11:48:05 +0100

    Extract wavs if dir doesn't exist

--- get_data.sh
+++ get_data.sh
@@ -19,14 +19,29 @@ if ! [ -d data ]; then
     rm mp3*
 fi
 
+if ! [ -d wav ]; then
+    nj=32
+    mkdir -p wav
+    ls data/a/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/b/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/c/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/d/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/e/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/f/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/0/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/1/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/2/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/3/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/4/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/5/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/6/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/7/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/8/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
+    ls data/9/*.mp3 | xargs -P $nj -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
 
-mkdir -p wav
-ls data/a/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
-ls data/b/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
-ls data/c/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
-ls data/d/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
-ls data/e/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
-ls data/f/*.mp3 | xargs -P 20 -I % sh -c 'f=`basename -s ".mp3" %`; ffmpeg -i % -acodec pcm_u8 -ar 16000 wav/$f.wav'
-
+    # These files had errors
+    rm wav/norine_braun-now_and_zen-08-gently-117-146.wav
+    rm wav/american_baroque-dances_and_suites_of_rameau_and_couperin-25-le_petit_rien_xiveme_ordre_couperin-88-117.wav
+fi
 
 echo "$0: Data downloaded successfully"
